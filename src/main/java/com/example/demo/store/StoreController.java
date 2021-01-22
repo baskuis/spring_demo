1551261229cff1980f94eae3b9e3ded6b395dcf8
Bas Kuis <b@ukora.com>
2021-01-22 13:20:27 -0600

    create product example

--- src/main/java/com/example/demo/store/StoreController.java
+++ src/main/java/com/example/demo/store/StoreController.java
@@ -2,8 +2,8 @@ package com.example.demo.store;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,13 +17,25 @@ public class StoreController {
         return "phone: 414.123.1234";
     }
 
-    @PostMapping(path = "/createProduct")
-    void createProduct(@RequestBody ProductEntity product) {
-        productEntityCrudRepository.save(product);
+    @GetMapping(path = "/createProduct", produces = "text/html")
+    String showProductForm() {
+        String output = "<form action='' method='POST'>";
+        output += "Name: <input name='name' type='text' /><br />";
+        output += "Price: <input name='price' type='text' /><br />";
+        output += "<input type='submit' />";
+        output += "</form>";
+        return output;
     }
 
-    void logSomeStuff() {
-
+    @PostMapping(path = "/createProduct")
+    void createProduct(@ModelAttribute ProductEntity product) {
+        if (product == null || product.getName() == null) {
+            throw new RuntimeException("Name required");
+        }
+        if (product.getPrice() < 0) {
+            throw new RuntimeException("Price cannot be negative");
+        }
+        productEntityCrudRepository.save(product);
     }
 
     @GetMapping(path = "/home")
@@ -41,11 +53,11 @@ public class StoreController {
 
         Iterable<ProductEntity> products = productEntityCrudRepository.findAll();
 
-        products.forEach((p) -> {
-            System.out.println(p.getName());
-        });
-
-        return "Hi home";
+        String myProducts = "<h2>Our products</h2>";
+        for (ProductEntity p: products) {
+            myProducts = myProducts + "<p>" + p.getName() + " $" + p.getPrice() + "</p>";
+        }
+        return myProducts;
     }
 
 }
